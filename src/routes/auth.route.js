9d094d7110a7390904711287d5358f663d603e04
amosmurmu <amosmurmu002@gmail.com>
2025-01-16 02:52:51 +0530

    initial commit

---
+++ src/routes/auth.route.js
@@ -0,0 +1,22 @@
+import {
+  signup,
+  login,
+  logout,
+  updateProfile,
+  check,
+} from '../controllers/auth.controller.js';
+import express from 'express';
+import { protectRoute } from '../middleware/auth.middleware.js';
+const router = express.Router();
+
+router.post('/signup', signup);
+
+router.post('/login', login);
+
+router.post('/logout', logout);
+
+router.put('/update-profile', protectRoute, updateProfile);
+
+router.get('/check', protectRoute, check);
+
+export default router;
